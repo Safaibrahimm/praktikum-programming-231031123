782b2b22bbf45b0410cbc7a545a3ee4268f63562
Safaibrahimm <153706911+Safaibrahimm@users.noreply.github.com>
2023-12-17 20:48:37 +0800

    Upload Percabangan 1

---
+++ 231031123-Safa-Percabangan1.py
@@ -0,0 +1,11 @@
+nilai = 90
+batas_lulus = 65
+if nilai > 65:
+ print('Selamat, kamu lulus!')
+else: 
+ print('kamu tidak lulus')
+if nilai < 65:
+ print('Selamat, kamu lulus!')
+else: 
+ print('kamu tidak lulus')
+
